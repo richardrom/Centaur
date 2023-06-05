feee25c77d6519e84f2e24a90ed20656ab1be560
Richard Romero <qricardo@outlook.com>
2023-06-05 15:47:06 -0600

    Set the UI default font Fix a potential memory leak

--- Centaur/src/main.cpp
+++ Centaur/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "CentaurApp.hpp"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
@@ -15,7 +16,25 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("centaur.com");
     QCoreApplication::setApplicationName("Centaur");
 
-    auto app = new CENTAUR_NAMESPACE::CentaurApp;
+#ifdef Q_OS_MAC
+    const QString fontFile = QCoreApplication::applicationDirPath() + "/../Resources/Fonts/Inter-VariableFont_slnt,wght.ttf";
+#endif
+
+    const auto id = QFontDatabase::addApplicationFont(fontFile);
+    if (id != -1)
+    {
+        const auto families = QFontDatabase::applicationFontFamilies(id);
+        QFont font(families);
+        QApplication::setFont(font);
+    }
+#ifndef QT_NO_DEBUG_OUTPUT
+    else
+    {
+        qDebug() << "Font could not be loaded";
+    }
+#endif
+
+    auto app = std::make_unique<CENTAUR_NAMESPACE::CentaurApp>();
     app->show();
 
     return QApplication::exec();
