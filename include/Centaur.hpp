e09ed39c12787742be4eca459c1fc17f7bad42b5
Richard Romero <qricardo@outlook.com>
2023-06-12 11:26:17 -0600

    New compiler macros identification

--- include/Centaur.hpp
+++ include/Centaur.hpp
@@ -13,14 +13,55 @@
 #ifndef CENTAUR_PROJECT
 #define CENTAUR_PROJECT
 
+#ifdef C_C_UNKNOWN
+// This behavior might change in the future as more compilers are testes
+#error "Centaur Unknown Compiler"
+#endif /*C_C_UNKNOWN*/
+
+// TODO: Replace macros to C_CLANG, C_GNU, C_GNU_CLANG or C_MSVC
+#if defined(C_C_CLANG)
+// These are cmake defined
+#define C_CLANG 1
+#else
+// These are compiler defined
+#if defined(__clang__)
+#define C_CLANG 1
+#endif /*__clang__*/
+#endif
+
+#if defined(C_C_GNU)
+#define C_GNU 1
+#else
+#if (defined(__GNUC__) && !defined(C_CLANG))
+#define C_GNU 1
+#endif /*__GNUC__*/
+#endif
+
+#if defined(C_C_MSVC)
+#define C_MSVC 1
+#else
+#if (defined(_MSC_VER) && !defined(C_CLANG) && !defined(C_GNU))
+#define C_MSVC 1
+#endif
+#endif /*C_C_MSVC*/
+
+#if defined(C_CLANG) || defined(C_GNU)
+#define C_GNU_CLANG 1
+#endif
+
+#if !defined(C_CLANG) && !defined(C_GNU) && !defined(C_MSVC)
+#error "Compiler could not be determinate"
+#else
+#define C_ALL_SUPPORTED 1
+#endif
+
 #define CENTAUR_VERSION_CODE(x, y, z) \
     (((x)*100000) + ((y)*100) + (z))
 
 #define CENTAUR_STR(x)    CENTAUR_DO_STR(x)
 #define CENTAUR_DO_STR(x) #x
 
-#if defined(__clang__) || defined(__GNUC__)
-
+#ifdef C_GNU_CLANG
 #define CENTAUR_PRAGMA(x)      _Pragma(CENTAUR_STR(x))
 #define CENTAUR_WARN_PRAGMA(x) CENTAUR_PRAGMA(GCC diagnostic x)
 #define CENTAUR_WARN_OFF(x)    CENTAUR_WARN_PRAGMA(ignored x)
