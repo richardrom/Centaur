eae59c6bdfb6d686da84ffa7bf838b929bad31b1
Richard Romero <qricardo@outlook.com>
2023-08-25 11:20:17 -0600

    Add platform dependent names for ctpack and the library file

--- scripts/theme-plpack.py
+++ scripts/theme-plpack.py
@@ -1,6 +1,7 @@
 import argparse
 import subprocess
 import sys
+import platform
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -31,9 +32,21 @@ if __name__ == '__main__':
         print('Library path is empty')
         sys.exit(1)
 
+    libraryName = ""
+    currentSystem = platform.system()
+    if currentSystem == "Linux":
+        libraryName = "libCentTheme.so"
+    elif currentSystem == "Windows":
+        libraryName = "CentTheme.dll"
+    elif currentSystem == "Darwin":
+        libraryName = "libCentTheme.dylib"
+    else:
+        libraryName = "libCentTheme"
+
     exit(subprocess.run("{0}/plpack --name=CtTheme"
                         " --version=0.1.0 --man=\"Centaur Project\" --min=0.1.0"
-                        " --lib=\"{1}/libCentTheme.dylib\" --out=\"{2}\""
+                        " --lib=\"{1}/{4}\" --out=\"{2}\""
                         " --uuid=\"2df95e88-7eae-5940-ab0d-b53f2df855e2\""
-                        " --theme --extra=\"{3}/dark.theme.json\"".format(args.bin, args.lib, args.output, args.local),
+                        " --theme --extra=\"{3}/dark.theme.json\"".format(args.bin, args.lib, args.output,
+                                                                          args.local, libraryName),
                         shell=True).returncode)
